905178f07ae515100de87e5525522b3458c63916
AshikaChadaga <ashikachadaga@gmail.com>
2021-09-05 20:27:03 +0530

    ADDED: Demonstration of add method

---
+++ src/com/bridgelabz/oop/commercialdataprocessing/Transaction.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.oop.commercialdataprocessing;
+
+public class Transaction {
+
+	public static void main(String[] args) {
+		
+		StockAccount Idea = new StockAccount("Idea", 100, 10, "1/2/2018 3:48");
+		StockAccount Reliance = new StockAccount("Reliance", 30, 100, "1/6/2018 4:48");
+		System.out.println(Idea);
+		System.out.println(Reliance);
+		
+
+	}
+
+}
